48600d7d8e6fc40bc839bcfaeb1f94cb07e50652
sanjidarimi023 <sanjidarimi023@gmail.com>
2025-12-25 16:49:04 +0600

    create user role hook

--- src/Hooks/useUserRole.jsx
+++ src/Hooks/useUserRole.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import { useAuth } from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const useUserRole = () => {
-//     const {user, loading }=useAuth()
-//    const axiosSecure = useAxiosSecure();
+  const { user, loading } = useAuth();
+  const axiosSecure = useAxiosSecure();
 
-//    const {data:role, isPending: roleLoading}=useQuery
-    return (
-        <>
-            
-        </>
-    );
+  const { data: role, isPending: roleLoading } = useQuery({
+    queryKey: ["user-role", user?.email],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/user/role?email=${user?.email}`);
+      return res.data?.role;
+    },
+  });
+
+  return { role, roleLoading };
 };
 
 export default useUserRole;
